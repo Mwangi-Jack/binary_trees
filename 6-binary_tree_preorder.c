44216ea9117035fe1cfaffb178c685ee1faf333d
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-27 11:58:55 +0300

    preorder traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "binary_trees.h"
 
 /**
@@ -12,7 +13,12 @@
  * Return: nothing (void)
 */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*funct)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
 
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
