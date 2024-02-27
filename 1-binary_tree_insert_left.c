77f884b245c7e37270a93a3f983f20542ea7b831
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-27 10:50:06 +0300

    inserting left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,19 +27,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = value;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
 	if (parent->left != NULL)
 	{
 		prev_node = parent->left;
-		new_node->parent = parent;
+		parent->left = new_node;
 		new_node->left = prev_node;
-		prev_node->parent = new_node;
 	}
 
-	new_node->right = NULL;
-	new_node->parent = parent;
-	new_node->n = value;
 	parent->left = new_node;
-
-
 	return (new_node);
 }
