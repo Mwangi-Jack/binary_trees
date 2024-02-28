82d04d0f407861777d898b77a84dbc577dabe666
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-28 12:39:59 +0300

    getting depth of a binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,16 +14,14 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth = 0;
-	size_t right_depth = 0;
+	size_t depth = 0;
 
 	if (tree == NULL)
 		return (0);
 
 
-	left_depth = tree->parent ? 1 + binary_tree_depth(tree->left) : 0;
-	right_depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 
-	return (MAX(left_depth, right_depth));
+	return (depth);
 
 }
