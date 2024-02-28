5de9fd16c5b68aa1f49e39209e90dddf5c6338be
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-28 17:20:22 +0300

    getting number of nodes with atleast one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,15 +15,15 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes  = 0;
+	size_t nodes = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	nodes += binary_tree_nodes(tree->left);
-	nodes +=  binary_tree_nodes(tree->right);
+	size_t left = binary_tree_nodes(tree->left);
+	size_t right =  binary_tree_nodes(tree->right);
 
-
+	nodes = left + right;
 
 	return ((tree->left || tree->right) ? nodes + 1 : nodes);
 }
