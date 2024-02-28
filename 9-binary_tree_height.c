b9b4799cd0dda78da8ee867d7be3fafd8bbba32b
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-28 11:21:49 +0300

    getting height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -21,13 +21,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 	{
 		return (0);
-	} else
-	{
-		if (tree)
-		{
-			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		}
-		return (MAX(left_height, right_height));
 	}
+
+	if (tree)
+	{
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	}
+	return (MAX(left_height, right_height));
+
 }
