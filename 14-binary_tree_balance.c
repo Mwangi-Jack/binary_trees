642feb2f7d2f47f1a21f44ac8bd0aee15cc576ff
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-02-29 11:34:24 +0300

    getting the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,12 +16,12 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height = 0, right_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	left_height = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
 	right_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-
 
 	return (left_height - right_height);
 }
